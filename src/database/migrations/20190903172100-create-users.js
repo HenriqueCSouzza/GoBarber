9a2019eb0bf7c09625d619e2688e1511f1c91969
HenriqueCSouzza <henrique.csouzza@gmail.com>
2019-09-03 21:46:26 -0300

    migrate

---
+++ src/database/migrations/20190903172100-create-users.js
@@ -0,0 +1,43 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      // criando os campos da tabela
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaulValue: false,
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  // utilizado para quando for dar rollback
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
+};
